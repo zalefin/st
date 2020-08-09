629d034ccc11ba565a214c3de424d07a36aa7fd9
Zach Lefin <zachlefin@gmail.com>
2020-08-09 16:41:42 -0400

    change color to gruvbox

--- config.h
+++ config.h
@@ -125,30 +125,52 @@ static const char *colorname[] = {
 	/* "#3971ed", */
 	/* "#ffffff", */
 
-    "#1c1c1c",
-    "#ff005b",
-    "#cee318",
-    "#ffe755",
-    "#048ac7",
-    "#b854df",
-    "#0ac1cd",
-    "#e5e5e5",
-    "#666666",
-    "#ff00a0",
-    "#ccff00",
-    "#ff9f00",
-    "#48c6ff",
-    "#d775ff",
-    "#63e7f0",
-    "#f3f3f3",
+    /* zach backup */
+    /* "#1c1c1c", */
+    /* "#ff005b", */
+    /* "#cee318", */
+    /* "#ffe755", */
+    /* "#048ac7", */
+    /* "#b854df", */
+    /* "#0ac1cd", */
+    /* "#e5e5e5", */
+    /* "#666666", */
+    /* "#ff00a0", */
+    /* "#ccff00", */
+    /* "#ff9f00", */
+    /* "#48c6ff", */
+    /* "#d775ff", */
+    /* "#63e7f0", */
+    /* "#f3f3f3", */
 
-	[255] = 0,
+	/* [255] = 0, */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#c5c5c5", // FG
-	"#1c1c1c", // BG
-	"#915a00", // cursor
-	"#6d00e3", // cursor reverse
+	/* /1* more colors can be added after 255 to use with DefaultXX *1/ */
+	/* "#c5c5c5", // FG */
+	/* "#1c1c1c", // BG */
+	/* "#915a00", // cursor */
+	/* "#6d00e3", // cursor reverse */
+
+
+    /* 8 normal colors */
+    [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+    [1] = "#cc241d", /* red     */
+    [2] = "#98971a", /* green   */
+    [3] = "#d79921", /* yellow  */
+    [4] = "#458588", /* blue    */
+    [5] = "#b16286", /* magenta */
+    [6] = "#689d6a", /* cyan    */
+    [7] = "#a89984", /* white   */
+
+    /* 8 bright colors */
+    [8]  = "#928374", /* black   */
+    [9]  = "#fb4934", /* red     */
+    [10] = "#b8bb26", /* green   */
+    [11] = "#fabd2f", /* yellow  */
+    [12] = "#83a598", /* blue    */
+    [13] = "#d3869b", /* magenta */
+    [14] = "#8ec07c", /* cyan    */
+    [15] = "#ebdbb2", /* white   */
 
 };
 
@@ -162,10 +184,10 @@ float alpha = .7;
 /* unsigned int defaultbg = 0; */
 /* static unsigned int defaultcs = 256; */
 /* static unsigned int defaultrcs = 257; */
-unsigned int defaultfg = 256;
-unsigned int defaultbg = 257;
-static unsigned int defaultcs = 8;
-static unsigned int defaultrcs = 259;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
